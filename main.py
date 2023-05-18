21bfb41a1210612c48ad95a0d1e99d86a4406df9
Abdelrahman Mohamed <78263461+Soooonic@users.noreply.github.com>
2023-05-19 02:01:44 +0300

    add searching algorithms

--- main.py
+++ main.py
@@ -4,6 +4,96 @@ import random
 ROW = 6
 COLUMN = 7
 
+def minmax(playground,player,depth):
+    if is_goal(playground):
+        winnerplayer=winner(playground)
+        #if the state is goal then the opposite player of the current player is winner
+        if(winnerplayer==1):
+            return None,4000
+        else:
+            return None,-4000
+    if(len(move(playground))==0):
+        return None,0
+    if(depth==0):
+        return None,hfun(playground)
+    if player==1:
+        cols=move(playground)
+        #dummy values
+        col=cols[0]
+        val = -100000000000000
+        for c in cols:
+            r=getrow(playground,c)
+            playground[r][c]=1
+            res=minmax(playground,2,depth-1)[1]
+            playground[r][c] = 0
+            if res > val:
+                val=res
+                col=c
+        return col,val
+
+    else:
+        cols = move(playground)
+        # dummy values
+        col = cols[0]
+        val = 100000000000000
+        for c in cols:
+            r = getrow(playground, c)
+            playground[r][c]=2
+            res = minmax(playground, 1,depth-1)[1]
+            playground[r][c]=0
+            if res < val:
+                val = res
+                col = c
+        return col, val
+
+def minmax_alphabeta(playground,player, alpha, beta,depth):
+    if is_goal(playground):
+        #if the state is goal then the opposite player of the current player is winner
+        if(winner(playground)==1):
+            return None,4000
+        else:
+            return None,-4000
+    if(len(move(playground))==0):
+        return None,0
+    if(depth==0):
+        return None,hfun(playground)
+    if player==1:
+        cols=move(playground)
+        #dummy values
+        col=cols[0]
+        val = -100000000000000
+        for c in cols:
+            r=getrow(playground,c)
+            playground[r][c]=1
+            res=minmax(playground,2,depth-1)[1]
+            playground[r][c] = 0
+            if res > val:
+                val=res
+                col=c
+            if(alpha<val):
+                alpha=val
+            if alpha >= beta:
+                break
+        return col,val
+
+    else:
+        cols = move(playground)
+        # dummy values
+        col = cols[0]
+        val = 100000000000000
+        for c in cols:
+            r = getrow(playground, c)
+            playground[r][c]=2
+            res = minmax(playground, 1,depth-1)[1]
+            playground[r][c]=0
+            if res < val:
+                val = res
+                col = c
+            if beta>val:
+                beta = val
+            if alpha >= beta:
+                break
+        return col, val
 
 def move(arr):
     cols = []
