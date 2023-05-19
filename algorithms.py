91e81eedcf61cd4d73c21766314100a0782b41f4
Abdelrahman Mohamed <78263461+Soooonic@users.noreply.github.com>
2023-05-19 23:19:37 +0300

    update score estimation

--- algorithms.py
+++ algorithms.py
@@ -2,32 +2,42 @@ from goal import is_goal
 from heuristic import *
 
 
-def minimax(playground, player, depth):
+import math
+import random
+import sys
+ROW = 6
+COLUMN = 7
+def minimax(playground,player,depth):
     if is_goal(playground):
-        winner = is_goal(playground)
-        # if the state is goal then the opposite player of the current player is winner
-        if winner == 1:
-            return None, 4000
+        #if the state is goal then the opposite player of the current player is winner
+        if (player == 1):
+            return None, -math.inf
         else:
-            return None, -4000
-    if len(move(playground)) == 0:
-        return None, 0
-    if depth == 0:
-        return None, h(playground)
-    if player == 1:
-        cols = move(playground)
-        # dummy values
-        col = cols[0]
+            return None, math.inf
+    if(len(move(playground))==0):
+        return None,0
+    if(depth==0):
+        return None,hfun(playground)
+    if player==1:
+        cols=move(playground)
+        #dummy values
+        col=cols[0]
         val = -100000000000000
         for c in cols:
-            r = get_row(playground, c)
-            playground[r][c] = 1
-            res = minimax(playground, 2, depth - 1)[1]
+            r = -1
+            for row in range(ROW):
+                if playground[row][c] == 0:
+                    r = row
+                    break
+            if (r == -1):
+                continue
+            playground[r][c]=1
+            res=minimax(playground,2,depth-1)[1]
             playground[r][c] = 0
             if res > val:
-                val = res
-                col = c
-        return col, val
+                val=res
+                col=c
+        return col,val
 
     else:
         cols = move(playground)
@@ -35,45 +45,56 @@ def minimax(playground, player, depth):
         col = cols[0]
         val = 100000000000000
         for c in cols:
-            r = get_row(playground, c)
-            playground[r][c] = 2
-            res = minimax(playground, 1, depth - 1)[1]
-            playground[r][c] = 0
+            r = -1
+            for row in range(ROW):
+                if playground[row][c] == 0:
+                    r = row
+                    break
+            if (r == -1):
+                continue
+            playground[r][c]=2
+            res = minimax(playground, 1,depth-1)[1]
+            playground[r][c]=0
             if res < val:
                 val = res
                 col = c
         return col, val
 
-
-def minimax_alphabeta(playground, player, alpha, beta, depth):
+def minimax_alphabeta(playground,player, alpha, beta,depth):
     if is_goal(playground):
-        # if the state is goal then the opposite player of the current player is winner
-        if is_goal(playground) == 1:
-            return None, 4000
+        #if the state is goal then the opposite player of the current player is winner
+        if (player == 1):
+            return None, -math.inf
         else:
-            return None, -4000
-    if len(move(playground)) == 0:
-        return None, 0
-    if depth == 0:
-        return None, h(playground)
-    if player == 1:
-        cols = move(playground)
-        # dummy values
-        col = cols[0]
+            return None, math.inf
+    if(len(move(playground))==0):
+        return None,0
+    if(depth==0):
+        return None,hfun(playground)
+    if player==1:
+        cols=move(playground)
+        #dummy values
+        col=cols[0]
         val = -100000000000000
         for c in cols:
-            r = get_row(playground, c)
-            playground[r][c] = 1
-            res = minimax(playground, 2, depth - 1)[1]
+            r=-1
+            for row in range(ROW):
+                if playground[row][c] == 0:
+                    r=row
+                    break
+            if(r==-1):
+                continue
+            playground[r][c]=1
+            res=minimax_alphabeta(playground,2,depth-1)[1]
             playground[r][c] = 0
             if res > val:
-                val = res
-                col = c
-            if alpha < val:
-                alpha = val
+                val=res
+                col=c
+            if(alpha<val):
+                alpha=val
             if alpha >= beta:
                 break
-        return col, val
+        return col,val
 
     else:
         cols = move(playground)
@@ -81,14 +102,20 @@ def minimax_alphabeta(playground, player, alpha, beta, depth):
         col = cols[0]
         val = 100000000000000
         for c in cols:
-            r = get_row(playground, c)
-            playground[r][c] = 2
-            res = minimax(playground, 1, depth - 1)[1]
-            playground[r][c] = 0
+            r = -1
+            for row in range(ROW):
+                if playground[row][c] == 0:
+                    r = row
+                    break
+            if (r == -1):
+                continue
+            playground[r][c]=2
+            res = minimax_alphabeta(playground, 1,depth-1)[1]
+            playground[r][c]=0
             if res < val:
                 val = res
                 col = c
-            if beta > val:
+            if beta>val:
                 beta = val
             if alpha >= beta:
                 break
--- heuristic.py
+++ heuristic.py
@@ -1,94 +1,69 @@
-ROW = 6
-COLUMN = 7
-
+ROW=6
+COLUMN=7
+def estimate(cntai ,cntcom,cntempty):
+    if cntai==4:
+       return 100
+    elif cntai==2 and cntempty==2:
+        return 2
+    elif cntai==3 and cntempty==1:
+        return 5
+    elif cntcom==3 and cntempty==1:
+        return -4
+    return 0
 
 def move(arr):
-    cols = []
+    cols=[]
     for c in range(COLUMN):
-        if arr[0][c] == 0:
+        if arr[ROW-1][c]==0:
             cols.append(c)
     return cols
 
 
-def get_row(arr, col):
-    for row in range(ROW)[::-1]:
-        if arr[row][col] == 0:
-            return row
-
-
-def check(r, c):
-    if 0 <= r < ROW and 0 <= c < COLUMN:
+def check(r,c):
+    if(r>=0 and c>=0 and r<ROW and c<COLUMN):
         return True
     else:
         return False
-
-
-def h(board):
+def hfun(board):
     score = 0
-    for row in range(ROW):
+    for row in range (ROW):
         for col in range(COLUMN):
-            if check(row + 3, col):
-                score = score + get_score_row(board, row, col)
-            if check(row, col + 3):
-                score = score + get_score_col(board, row, col)
-            if check(row + 3, col - 3):
-                score = score + get_score_secondary_diagonal(board, row, col)
-            if check(row + 3, col + 3):
-                score = score + get_score_main_diagonal(board, row, col)
+            if(check(row+3,col)):
+                score=score+getscore_row(board,row,col)
+            if(check(row,col+3)):
+                score=score+getscore_col(board,row,col)
+            if(check(row+3,col+3)):
+                score =score+ getscore_secondary_diagonal(board, row, col)
+            if(check(row+3,col-3)):
+                score=score+getscore_main_diagonal(board,row,col)
+    center = []
+    mid=COLUMN // 2
+    for i in range(ROW):
+       center.append(board[i][mid])
+    cnt = center.count(1)*3
+    score=score+ cnt
     return score
 
+def getscore_row(board,row,col):
+    cntai=(board[row][col]==1)+(board[row+1][col]==1)+(board[row+2][col]==1)+(board[row+3][col]==1)
+    cntcom=(board[row][col]==2)+(board[row+1][col]==2)+(board[row+2][col]==2)+(board[row+3][col]==2)
+    cntempty=(board[row][col]==0)+(board[row+1][col]==0)+(board[row+2][col]==0)+(board[row+3][col]==0)
+    return estimate(cntai, cntcom, cntempty)
 
-def get_score_row(board, row, col):
-    cnt_ai = (board[row][col] == 1) + (board[row + 1][col] == 1) + (board[row + 2][col] == 1) + (
-            board[row + 3][col] == 1)
-    cnt_com = (board[row][col] == 2) + (board[row + 1][col] == 2) + (board[row + 2][col] == 2) + (
-            board[row + 3][col] == 2)
-    cnt_empty = (board[row][col] == 0) + (board[row + 1][col] == 0) + (board[row + 2][col] == 0) + (
-            board[row + 3][col] == 0)
-    return estimate(cnt_ai, cnt_com, cnt_empty)
+def getscore_col(board,row,col):
+    cntai=(board[row][col]==1)+(board[row][col+1]==1)+(board[row][col+2]==1)+(board[row][col+3]==1)
+    cntcom=(board[row][col]==2)+(board[row][col+1]==2)+(board[row][col+2]==2)+(board[row][col+3]==2)
+    cntempty=(board[row][col]==0)+(board[row][col+1]==0)+(board[row][col+2]==0)+(board[row][col+3]==0)
+    return estimate(cntai, cntcom, cntempty)
 
+def getscore_main_diagonal(board,row,col):
+    cntai=(board[row][col]==1)+(board[row+1][col-1]==1)+(board[row+2][col-2]==1)+(board[row+3][col-3]==1)
+    cntcom=(board[row][col]==2)+(board[row+1][col-1]==2)+(board[row+2][col-2]==2)+(board[row+3][col-3]==2)
+    cntempty=(board[row][col]==0)+(board[row+1][col-1]==0)+(board[row+2][col-2]==0)+(board[row+3][col-3]==0)
+    return estimate(cntai, cntcom, cntempty)
 
-def get_score_col(board, row, col):
-    cnt_ai = (board[row][col] == 1) + (board[row][col + 1] == 1) + (board[row][col + 2] == 1) + (
-            board[row][col + 3] == 1)
-    cnt_com = (board[row][col] == 2) + (board[row][col + 1] == 2) + (board[row][col + 2] == 2) + (
-            board[row][col + 3] == 2)
-    cnt_empty = (board[row][col] == 0) + (board[row][col + 1] == 0) + (board[row][col + 2] == 0) + (
-            board[row][col + 3] == 0)
-    return estimate(cnt_ai, cnt_com, cnt_empty)
-
-
-def get_score_secondary_diagonal(board, row, col):
-    cnt_ai = (board[row][col] == 1) + (board[row + 1][col - 1] == 1) + (board[row + 2][col - 2] == 1) + (
-            board[row + 3][col - 3] == 1)
-    cnt_com = (board[row][col] == 2) + (board[row + 1][col - 1] == 2) + (board[row + 2][col - 2] == 2) + (
-            board[row + 3][col - 3] == 2)
-    cnt_empty = (board[row][col] == 0) + (board[row + 1][col - 1] == 0) + (board[row + 2][col - 2] == 0) + (
-            board[row + 3][col - 3] == 0)
-    return estimate(cnt_ai, cnt_com, cnt_empty)
-
-
-def get_score_main_diagonal(board, row, col):
-    cnt_ai = (board[row][col] == 1) + (board[row + 1][col + 1] == 1) + (board[row + 2][col + 2] == 1) + (
-            board[row + 3][col + 3] == 1)
-    cnt_com = (board[row][col] == 2) + (board[row + 1][col + 1] == 2) + (board[row + 2][col + 2] == 2) + (
-            board[row + 3][col + 3] == 2)
-    cnt_empty = (board[row][col] == 0) + (board[row + 1][col + 1] == 0) + (board[row + 2][col + 2] == 0) + (
-            board[row + 3][col + 3] == 0)
-    return estimate(cnt_ai, cnt_com, cnt_empty)
-
-
-def estimate(cnt_ai, cnt_com, cnt_empty):
-    if cnt_ai == 4:
-        return 1000
-    elif cnt_ai == 2 and cnt_empty == 2:
-        return 500
-    elif cnt_ai == 3 and cnt_empty == 1:
-        return 750
-    elif cnt_com == 4:
-        return -1000
-    elif cnt_com == 2 and cnt_empty == 2:
-        return -500
-    elif cnt_com == 3 and cnt_empty == 1:
-        return -750
-    return 0
+def getscore_secondary_diagonal(board,row,col):
+    cntai=(board[row][col]==1)+(board[row+1][col+1]==1)+(board[row+2][col+2]==1)+(board[row+3][col+3]==1)
+    cntcom=(board[row][col]==2)+(board[row+1][col+1]==2)+(board[row+2][col+2]==2)+(board[row+3][col+3]==2)
+    cntempty=(board[row][col]==0)+(board[row+1][col+1]==0)+(board[row+2][col+2]==0)+(board[row+3][col+3]==0)
+    return estimate(cntai ,cntcom,cntempty)
